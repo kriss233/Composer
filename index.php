bc049658b93de0f3d6c9262f1b9af4511473c70f
Kristof Olah <olahkris@gmail.com>
2023-04-13 10:54:10 +0200

    variable is getting dumped

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 
 //composer config sort-packages true
 
+use Symfony\Component\Dotenv\Dotenv;
+
 require __DIR__ . "/vendor/autoload.php";
 
-dump($_SERVER);
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/test.env');
+
+dump($_ENV);
